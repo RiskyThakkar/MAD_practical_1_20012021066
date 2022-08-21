73083473d2d040e2f115f29a966bd21ed84310a8
Smit Thakkar <smitthakkar20@gnu.ac.in>
2022-08-22 00:18:00 +0530

    Practical1_4.kt

---
+++ src/main/kotlin/Practical1_4.kt
@@ -0,0 +1,19 @@
+import javax.print.attribute.IntegerSyntax
+
+//Question : 4
+/*Find the number is odd or even by using Control Flow inside println() method.*/
+
+fun main()
+{
+    println("Enter any value: ")
+    var s1:Int = readLine()!!.toInt()
+
+    if(s1%2==0)
+    {
+        print("The number is Even....")
+    }
+    else
+    {
+        print("The number is Odd....")
+    }
+}
