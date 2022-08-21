437dd8d44fb8ca4683708e16a9aa0d43b8a6c806
Smit Thakkar <smitthakkar20@gnu.ac.in>
2022-08-22 00:18:46 +0530

    Practical1_5.kt

---
+++ src/main/kotlin/Practical1_5.kt
@@ -0,0 +1,24 @@
+fun main()
+{
+    print("Enter month number : ")
+    var mon:Int = readLine()!!.toInt()
+    when (mon) {
+        1 -> print("MONTH = January")
+        2 -> print("MONTH = February")
+        3 -> print("MONTH = March")
+        4 -> print("MONTH = April")
+        5 -> print("MONTH = May")
+        6 -> print("MONTH = June")
+        7 -> print("MONTH = July")
+        8 -> print("MONTH = August")
+        9 -> print("MONTH = September")
+        10 -> print("MONTH = October")
+        11-> print("MONTH = November")
+        12 -> print("MONTH = December")
+        else -> {
+            print("Enter proper month number")
+        }
+    }
+}
+
+
