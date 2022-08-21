6ed44714a06f0a64f9f96ccc20c7ec0f2442f74e
Smit Thakkar <smitthakkar20@gnu.ac.in>
2022-08-22 00:13:54 +0530

    Practical1_1.kt

---
+++ src/main/kotlin/Practical1_1.kt
@@ -0,0 +1,27 @@
+//Question : 1
+
+/*Store & Display values in different variable of different type (Integer, Double,
+Float, Long, Short, Byte, Char, Boolean, String)*/
+
+fun main()
+{
+    val a : Int = 678;
+    val b : Double = 256.321;
+    val c : Float = 45.12f;
+    val d : Long = 256974564121;
+    val e : Short = 75;
+    val f : Byte = 9;
+    val g : Char = 'S';
+    val h : Boolean = true;
+    val i : String = "SMIT";
+
+    println("Integer Value = $a");
+    println("Double Value = $b");
+    println("Float Value = $c");
+    println("Long Value = $d");
+    println("Short Value = $e");
+    println("Byte Value = $f");
+    println("Char Value = $g");
+    println("Boolean Value = $h");
+    println("String Value = $i");
+}
