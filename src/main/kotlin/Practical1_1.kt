024386a697d88ece7f272821fb721d7f4faee366
Smit Thakkar <smitthakkar20@gnu.ac.in>
2022-08-22 23:31:49 +0530

    Practical1_1.kt

--- src/main/kotlin/Practical1_1.kt
+++ src/main/kotlin/Practical1_1.kt
@@ -5,23 +5,23 @@ Float, Long, Short, Byte, Char, Boolean, String)*/
 
 fun main()
 {
-    val a : Int = 678;
-    val b : Double = 256.321;
-    val c : Float = 45.12f;
-    val d : Long = 256974564121;
-    val e : Short = 75;
-    val f : Byte = 9;
-    val g : Char = 'S';
-    val h : Boolean = true;
-    val i : String = "SMIT";
+    val a : Int = 678
+    val b : Double = 256.321
+    val c : Float = 45.12f
+    val d : Long = 256974564121
+    val e : Short = 75
+    val f : Byte = 9
+    val g : Char = 'S'
+    val h : Boolean = true
+    val i : String = "smit"
 
-    println("Integer Value = $a");
-    println("Double Value = $b");
-    println("Float Value = $c");
-    println("Long Value = $d");
-    println("Short Value = $e");
-    println("Byte Value = $f");
-    println("Char Value = $g");
-    println("Boolean Value = $h");
-    println("String Value = $i");
+    println("Integer Value = $a")
+    println("Double Value = $b")
+    println("Float Value = $c")
+    println("Long Value = $d")
+    println("Short Value = $e")
+    println("Byte Value = $f")
+    println("Char Value = $g")
+    println("Boolean Value = $h")
+    println("String Value = $i")
 }
