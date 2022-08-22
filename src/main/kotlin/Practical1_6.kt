5ba1a0c9b369c97e8fe670a27d826c54aa4380bf
Smit Thakkar <smitthakkar20@gnu.ac.in>
2022-08-22 21:38:54 +0530

    Practical1_6.kt

---
+++ src/main/kotlin/Practical1_6.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    cal(111,2222,-222)
+}
+fun cal(a:Int, b:Int, c:Int)
+{
+    println("Addition of $a, $b & $c : ${a+b+c}")
+    println("Subtraction of $a, $b & $c : ${a-b-c}")
+    println("Multiplication of $a, $b & $c : ${a*b*c}")
+    println("Division of $b & $a : ${b/a}")
+}
