67f4846ce88880fbdfd76975ef056ccaf79d7b75
Smit Thakkar <smitthakkar20@gnu.ac.in>
2022-08-22 23:32:46 +0530

    Practical1_3.kt

--- src/main/kotlin/Practical1_3.kt
+++ src/main/kotlin/Practical1_3.kt
@@ -1,26 +1,26 @@
 fun main()
 {
     println ("Student Enrollment Number : ")
-    var enro:String = readLine()!!.toString()
+    val enro:String = readLine()!!.toString()
 
     println ("Student Name : ")
-    var nm:String = readLine()!!.toString()
+    val nm:String = readLine()!!.toString()
 
     println ("Student Branch : ")
-    var branch:String = readLine()!!.toString()
+    val branch:String = readLine()!!.toString()
 
     println ("Student Batch : ")
-    var bat:String = readLine()!!.toString()
+    val bat:String = readLine()!!.toString()
 
     println ("Student College Name : ")
-    var clgnm:String = readLine()!!.toString()
+    val clgnm:String = readLine()!!.toString()
 
     println ("Student University Name : ")
 
-    var uninm:String = readLine()!!.toString()
+    val uninm:String = readLine()!!.toString()
 
     println ("Student Age : ")
-    var age = readLine()!!.toInt()
+    val age = readLine()!!.toInt()
 
 
     println("********************")
